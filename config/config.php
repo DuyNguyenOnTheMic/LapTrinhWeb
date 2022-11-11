4d55769f5d8135ba0b4e4614edcee284aebb515d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-11 16:20:35 +0700

    Add dbconnect config

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php 
+// Common settings 
+define('CURRENCY', 'VND'); 
+define('CURRENCY_SYMBOL', '$');
+  
+// Database configuration  
+define('DB_HOST', 'localhost'); 
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', ''); 
+define('DB_NAME', 'laptopnganngo');
