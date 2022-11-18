7bc2f9b4eecbb33c7a27877f361485959c05ff35
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-18 20:03:33 +0700

    Delete redundant html

--- cart.php
+++ cart.php
@@ -127,54 +127,11 @@ $cart = new Cart;
     </div>
 </div>
 <!-- SHOPING CART AREA END -->
-
-<!-- BRAND LOGO AREA START -->
-<div class="ltn__brand-logo-area  ltn__brand-logo-1 section-bg-1 pt-35 pb-35 plr--5">
-    <div class="container-fluid">
-        <div class="row ltn__brand-logo-active">
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/1.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/2.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/3.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/4.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/5.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/1.png" alt="Brand Logo">
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="ltn__brand-logo-item">
-                    <img src="img/brand-logo/2.png" alt="Brand Logo">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- BRAND LOGO AREA END -->
 </div>
 <!-- Body main wrapper end -->
 
 <?php require_once 'includes/footer.php'; ?>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-touchspin/4.3.0/jquery.bootstrap-touchspin.min.js"></script>
 <script>
     $("input[name='quantity']").TouchSpin({
@@ -197,10 +154,10 @@ $cart = new Cart;
             }
         });
 
-    $('.cart-product-remove').on('click', function(e) {
-        e.preventDefault();
-        alert('hehe');
-    });
+        $('.cart-product-remove').on('click', function(e) {
+            e.preventDefault();
+            alert('hehe');
+        });
     }
 </script>
 
