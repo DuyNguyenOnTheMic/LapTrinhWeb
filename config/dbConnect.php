f8cabfc4c4c2160f60763ec590b177181bbdfd85
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-19 21:18:12 +0700

    Update dbconnect

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -1,12 +1,27 @@
-<?php 
-// Include the configuration file 
+<?php
 require_once 'config.php'; 
- 
-// Connect with the database  
-$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);  
-  
-// Display error if failed to connect  
-if ($db->connect_errno) {  
-    printf("Connect failed: %s\n", $db->connect_error);  
-    exit();  
+class Db {
+    // variable uses to connect db
+    protected static $connection;
+
+    // function uses to connect
+    public function Connect() {
+        if (!isset(self::$connection)) {
+            self::$connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);  
+        }
+
+        // handle error when db connect failed
+        if (self::$connection == false) {
+            return false;
+        }
+        return self::$connection;
+    }
+
+    // function uses to execute query
+    public function QueryExecute($queryString) {
+        $connection = $this->Connect();
+        $result = $connection->query($queryString);
+        $connection->close();
+        return $result;
+    }
 }
