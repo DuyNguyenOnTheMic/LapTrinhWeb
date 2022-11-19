48cc1f698e00c099c94f9f72d5e1a05d08872b38
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-19 21:40:03 +0700

    Update login config for admin

---
+++ admin/login-config/db.class.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php'; 
+
+class Db {
+    // variable uses to connect db
+    protected static $connection;
+
+    // function uses to connect
+    public function Connect() {
+        if (!isset(self::$connection)) {
+            self::$connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        }
+
+        // handle error when db connect failed
+        if (self::$connection == false) {
+            return false;
+        }
+        return self::$connection;
+    }
+
+    // function uses to execute query
+    public function QueryExecute($queryString) {
+        $connection = $this->Connect();
+        $result = $connection->query($queryString);
+        $connection->close();
+        return $result;
+    }
+}
--- config/dbConnect.php
+++ config/dbConnect.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // Include the configuration file 
-require_once 'config.php'; 
- 
+require_once 'config.php';
+
 // Connect with the database  
-$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);  
-  
+$db = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 // Display error if failed to connect  
-if ($db->connect_errno) {  
-    printf("Connect failed: %s\n", $db->connect_error);  
-    exit();  
+if ($db->connect_errno) {
+    printf("Connect failed: %s\n", $db->connect_error);
+    exit();
 }
