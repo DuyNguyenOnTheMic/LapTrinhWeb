33b1f9db097939156fb2b9e410eb1261079b7d8a
QuocNam <tranquocnam201@gmail.com>
2022-11-20 21:45:47 +0700

    tạo tempalte

--- admin/user.php
+++ admin/user.php
@@ -1,103 +1,73 @@
-<?php require_once 'includes/header.php';
+<?php
+require_once 'includes/header.php';
+require_once '../includes/compress.php';
+
 require_once '../config/dbConnect.php';
-
-
-$sql = "SELECT * FROM products";
-$query = mysqli_query($db, $sql);
+$sqlQ = "SELECT * FROM `users`";
+$stmt = $db->prepare($sqlQ);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
 
 <!-- BEGIN: Content -->
 <div class="content">
     <h2 class="intro-y text-lg font-medium mt-10">
-        Danh sách sản phẩm
+        User List
     </h2>
-    <div class="grid grid-cols-12 gap-6 mt-5">
-        <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-            <button class="btn btn-primary shadow-md mr-2"> <a href="addProduct.php">Add product</a> </button>
+    <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
+            <button class="btn btn-primary shadow-md mr-2"> <a href="addUser.php">Add User</a> </button>
 
-            <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
-                <div class="w-56 relative text-slate-500">
-                    <input type="text" class="form-control w-56 box pr-10" placeholder="Search...">
-                    <i class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0" data-lucide="search"></i>
-                </div>
-            </div>
+          
         </div>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+
         <!-- BEGIN: Data List -->
         <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
             <table class="table table-report -mt-2">
                 <thead>
                     <tr>
-                        <th class="whitespace-nowrap">Image</th>
-                        <th class="whitespace-nowrap">Name</th>
-                        <th class="text-center whitespace-nowrap">Description</th>
-                        <th class="text-center whitespace-nowrap">Price</th>
-                        <th class="text-center whitespace-nowrap">Date created</th>
-                        <th class="text-center whitespace-nowrap">State</th>
-                        <th class="text-center whitespace-nowrap">Actions</th>
+                        <th class="whitespace-nowrap">ID</th>
+                        <th class=" text-center whitespace-nowrap">Name</th>
+                        <th class="text-center whitespace-nowrap">Email</th>
+                        <th class="text-center whitespace-nowrap">ACTIONS</th>
                     </tr>
                 </thead>
                 <tbody>
-
                     <?php
-                    while ($row = mysqli_fetch_array($query)) {
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
                     ?>
-                        <tr class="intro-x">
-                            <td class="w-40">
-                                <div class="flex">
-                                    <div class="w-10 h-10 image-fit zoom-in">
-                                        <img alt="Midone - HTML Admin Template" class="tooltip rounded-full" title="Uploaded at 12 September 2020" src='./images/<?= $row["image"] ?>'>
+                            <tr class="intro-x">
+                                <td>
+                                    <div class="flex">
+                                        <?php echo $row["id"]; ?>
                                     </div>
-                                </div>
-                            </td>
-                            <td><a href="" class="font-medium whitespace-nowrap"><?= $row["name"] ?></a></td>
-                            <td class="text-center"><?= $row["description"] ?></td>
-                            <td class="text-center"><?= $row["price"] ?> $ &nbsp;</td>
-                            <td><?= $row["created"] ?></td>
-                            <td class="text-center"><?= $row["status"] ?></td>
-
-                            <td class="table-report__action w-56">
+                                </td>
+                              
+                                <td class="text-center"><?php echo $row["first_name"]; ?> <?php echo $row["last_name"]; ?></td>
+                                
+                                <td class="text-center">
+                                    <?php echo $row["email"]; ?>
+                                </td>
+                              
+                                <td class="table-report__action w-56">
                                 <div class="flex justify-center items-center">
                                     <a class="flex items-center mr-3" href="updateproduct.php?id=<?= $row['id'] ?>"> <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Edit </a>
                                     <a class="flex items-center text-danger btn-delete" href="javascript:;" data-id="<?= $row['id'] ?>" data-tw-toggle="modal" data-tw-target="#delete-confirmation-modal"> <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Delete </a>
                                 </div>
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
+                        <?php }
+                    } else { ?>
+                        <p>Order(s) not found.....</p>
                     <?php } ?>
+
+
                 </tbody>
             </table>
         </div>
         <!-- END: Data List -->
-        <!-- BEGIN: Pagination -->
-        <div class="intro-y col-span-12 flex flex-wrap sm:flex-row sm:flex-nowrap items-center">
-            <nav class="w-full sm:w-auto sm:mr-auto">
-                <ul class="pagination">
-                    <li class="page-item">
-                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevrons-left"></i> </a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevron-left"></i> </a>
-                    </li>
-                    <li class="page-item"> <a class="page-link" href="#">...</a> </li>
-                    <li class="page-item active"> <a class="page-link" href="#">1</a> </li>
-                    <li class="page-item"> <a class="page-link" href="#">2</a> </li>
-                    <li class="page-item"> <a class="page-link" href="#">3</a> </li>
-                    <li class="page-item"> <a class="page-link" href="#">...</a> </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevron-right"></i> </a>
-                    </li>
-                    <li class="page-item">
-                        <a class="page-link" href="#"> <i class="w-4 h-4" data-lucide="chevrons-right"></i> </a>
-                    </li>
-                </ul>
-            </nav>
-            <select class="w-20 form-select box mt-3 sm:mt-0">
-                <option>10</option>
-                <option>25</option>
-                <option>35</option>
-                <option>50</option>
-            </select>
-        </div>
-        <!-- END: Pagination -->
+
     </div>
     <!-- BEGIN: Delete Confirmation Modal -->
     <div id="delete-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
@@ -115,7 +85,7 @@ $query = mysqli_query($db, $sql);
                     </div>
                     <div class="px-5 pb-8 text-center">
                         <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
-                        <button type="button" class="btn btn-danger w-24" id="btn-confirm">Delete</button>
+                        <button type="button" class="btn btn-danger w-24">Delete</button>
                     </div>
                 </div>
             </div>
