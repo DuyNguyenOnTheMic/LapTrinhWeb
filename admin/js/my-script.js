1a2f1ef3f7eae61c1fff5b81d02498832014db8a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-20 20:11:50 +0700

    Update post image to controller

--- admin/js/my-script.js
+++ admin/js/my-script.js
@@ -1,4 +1,4 @@
-$('#add-product-form').submit(function(e) {
+$('#add-product-form').submit(function (e) {
     e.preventDefault();
 
     // Get description text
@@ -9,14 +9,19 @@ $('#add-product-form').submit(function(e) {
     var $form = $(this);
     var serializedData = $form.serialize();
 
+    var data = new FormData(this); // <-- 'this' is your form element
+
     $.ajax({
         url: location.href,
         type: 'POST',
-        data: serializedData
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
     })
 });
 
-var loadFile = function(event) {
+var loadFile = function (event) {
     var image = document.getElementById('imgOutput');
     image.src = URL.createObjectURL(event.target.files[0]);
 };
