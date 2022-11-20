99ace67c003b6ec15c05290cf0e7771baec21133
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-20 18:27:15 +0700

    Update myscript.js

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,8 +1,9 @@
 <!-- BEGIN: JS Assets-->
-<script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
 <script src="js/app.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <!-- END: JS Assets-->
 <script src="js/ckeditor-classic.js"></script>
+<script src="js/my-script.js"></script>
 </body>
 
 </html>
