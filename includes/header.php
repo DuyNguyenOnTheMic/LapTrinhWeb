f59e3dfe874080808d8f1866f8cbf6ba803735e2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-21 11:10:35 +0700

    Update logo image

--- includes/header.php
+++ includes/header.php
@@ -46,7 +46,7 @@ $cart = new Cart;
                     <div class="row">
                         <div class="col">
                             <div class="site-logo">
-                                <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
+                                <a href="index.php"><img src="img/brand-logo/app-logo.png" width="100" alt="Logo"></a>
                             </div>
                         </div>
                         <div class="col header-contact-serarch-column d-none d-xl-block">
--- js/my-script.js
+++ js/my-script.js
@@ -1,1 +1,50 @@
-function updateCartItem(t,o){$.get("cartAction.php",{action:"updateCartItem",id:o,qty:t.value},(function(t){"ok"==t?location.reload():alert("Cart update failed, please try again.")}))}$(".btnAddToCart").on("click",(function(t){toastr.options.positionClass="toast-bottom-right",toastr.success("Thêm thành công!"),t.preventDefault(),$.ajax({url:$(this).attr("href")}).done((function(t){location.reload()}))})),$(".cart-product-remove").on("click",(function(t){if(1!=confirm("Are you sure to remove cart item?"))return!1;var o=$(this).data("id");$.ajax({url:"cartAction.php?action=removeCartItem&id="+o}).done((function(t){location.reload()}))})),$("input[name='quantity']").TouchSpin({min:0,max:100,buttondown_class:"btn theme-btn-1 border-0 px-4",buttonup_class:"btn theme-btn-1 border-0 px-4"}),$("#btnHehe").on("click",(function(){orderForm.submit()}));
+$('.btnAddToCart').on('click', function (e) {
+    toastr.options.positionClass = 'toast-bottom-right';
+    toastr.success('Thêm thành công!');
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr('href'),
+    }).done(function (data) {
+        location.reload();
+    });
+});
+
+$('.cart-product-remove').on('click', function (e) {
+    var result = confirm('Are you sure to remove cart item?');
+    if (result == true) {
+        // Delete cart item
+        var id = $(this).data('id');
+        $.ajax({
+            url: 'cartAction.php?action=removeCartItem&id=' + id,
+        }).done(function (data) {
+            location.reload();
+        });
+    } else {
+        return false;
+    }
+});
+
+$("input[name='quantity']").TouchSpin({
+    min: 0,
+    max: 100,
+    buttondown_class: "btn theme-btn-1 border-0 px-4",
+    buttonup_class: "btn theme-btn-1 border-0 px-4"
+});
+
+$('#btnHehe').on('click', function () {
+    orderForm.submit();
+});
+
+function updateCartItem(obj, id) {
+    $.get("cartAction.php", {
+        action: "updateCartItem",
+        id: id,
+        qty: obj.value
+    }, function (data) {
+        if (data == 'ok') {
+            location.reload();
+        } else {
+            alert('Cart update failed, please try again.');
+        }
+    });
+}
